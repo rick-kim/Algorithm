e8f04e12085440cdd99c0565c8378db153a8bc52
Rick <kbl0724@gmail.com>
2020-04-11 11:41:16 +0100

    Add project files.

---
+++ Queens/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Queens
+{
+    static class Program
+    {
+        private static int _successCount = 0;
+        static void Main(string[] args)
+        {
+            var task = new Task(6);
+            task.Success += Task_Success;
+            task.Fail += Task_Fail;
+            task.Start();
+
+            Console.WriteLine($"Total success case is {_successCount}");
+        }
+
+        private static void Task_Fail(int level, int[] result)
+        {
+            Console.WriteLine("Tracking fail.");
+            for (int k = 1; k < result.Length; k++)
+            {
+                Console.WriteLine($"level [{k}] => {result[k]}");
+            }
+            Console.WriteLine(Environment.NewLine);
+        }
+
+        private static void Task_Success(int[] result)
+        {
+            Console.WriteLine("Tracking success.");
+            for (int k = 1; k < result.Length; k++)
+            {
+                Console.WriteLine($"level [{k}] => {result[k]}");
+            }
+            Console.WriteLine(Environment.NewLine);
+            _successCount++;
+        }
+    }
+}
---
+++ Queens/Task.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Queens
+{
+    delegate void TrackingSuccess(int[] result);
+
+    delegate void TrackingFail(int level, int[] result);
+    class Task
+    {
+        public event TrackingSuccess Success;
+        public event TrackingFail Fail;
+
+        private readonly int _size;
+        private readonly List<int[]> _result;
+
+        public Task(int size)
+        {
+            _size = size;
+            _result = new List<int[]>();
+        }
+
+        public void Start()
+        {
+            for (int i = 1; i <= _size; i++)
+            {
+                var column = new int[_size + 1];
+                column[1] = i;
+                Tracking(1, column);
+            }
+        }
+
+        private bool Tracking(int level, int[] column)
+        {
+            if (!IsCanBePlaced(level, column))
+            {
+                // fail
+                Fail?.Invoke(level, column);
+                return false;
+            }
+
+            if (level == _size)
+            {
+                // success
+                Success?.Invoke(column);
+                return true;
+            }
+
+            // tracking child cases
+            for (int i = 1; i <= _size; i++)
+            {
+                column[level + 1] = i;
+                if (Tracking(level + 1, column))
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        // check if it is possible or not to place
+        private bool IsCanBePlaced(int level, int[] column)
+        {
+            if (level == 1)
+            {
+                return true;
+            }
+
+            for (int i = 1; i < level; i++)
+            {
+                if (column[i] == column[level])
+                {
+                    return false;
+                }
+
+                if (level - i == Math.Abs(column[level] - column[i]))
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
